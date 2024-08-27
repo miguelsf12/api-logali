97cea000a14c626d0859b8ed21e157efde720ed2
Miguel Santos <miguelsantosf12@gmail.com>
2024-08-27 16:28:02 -0300

    feat: servidor iniciado

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-const a = "teste a"
+require("dotenv").config()
+
+const express = require("express")
+
+const authRoutes = require("./users/routes/authRoutes")
+
+const app = express()
+
+app.use(express.json())
+
+app.use(express.static("public"))
+
+// Rotas
+app.use("/auth", authRoutes)
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000")
+})
