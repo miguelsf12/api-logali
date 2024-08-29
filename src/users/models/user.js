20550f53c9432b7e02bce63b610dba974c5057fc
Miguel Santos <miguelsantosf12@gmail.com>
2024-08-29 16:27:45 -0300

    feat: model User criado

---
+++ src/users/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("../../db/conn")
+
+const { Schema } = mongoose
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  cpf: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+
+const User = mongoose.model("User", UserSchema)
+
+module.exports = User
