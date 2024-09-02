417fe253dec587db269987a580b2dd01df5890f5
Miguel Santos <miguelsantosf12@gmail.com>
2024-09-02 18:18:24 -0300

    feat: checar parametros invalidos no login

---
+++ src/validators/login/login-invalid-params.js
@@ -0,0 +1,19 @@
+const InvalidParamError = require("../../errors/invalid-param-error")
+const User = require("../../users/models/user")
+const validator = require("validator")
+
+const loginInvalidParams = async (identifier) => {
+  const existingUser = await User.findOne({
+    $or: [{ email: identifier }, { cpf: identifier }],
+  })
+
+  if (!existingUser) {
+    let field = "email"
+    if (!validator.isEmail(identifier)) {
+      field = "cpf"
+    }
+    throw new InvalidParamError(field)
+  }
+}
+
+module.exports = loginInvalidParams
