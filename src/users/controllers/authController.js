c946256d8d8c16b5f58065ffacf7e2b87a519ceb
Miguel Santos <miguelsantosf12@gmail.com>
2024-09-02 18:19:23 -0300

    feat: criado o metodo de login

--- src/users/controllers/authController.js
+++ src/users/controllers/authController.js
@@ -1,11 +1,15 @@
 const User = require("../models/user")
+const validator = require("validator")
+const bcrypt = require("bcrypt")
 const checkMissingParams = require("../../validators/check-missing-params")
-const checkInvalidParams = require("../../validators/check-invalid-params")
+const signupInvalidParams = require("../../validators/signup/signup-invalid-params")
 const passwordValidator = require("../../validators/password-validator")
 const cpfValidator = require("../../validators/cpf-validator")
-const bcrypt = require("bcrypt")
-const cpfFormat = require("../../helpers/cpf-format")
 const emailValidator = require("../../validators/email-validator")
+const loginInvalidParams = require("../../validators/login/login-invalid-params")
+const cpfFormat = require("../../helpers/cpf-format")
+const checkPassword = require("../../helpers/check-password")
+const createUserToken = require("../../helpers/create-user-token")
 
 module.exports = class userController {
   static async register(req, res) {
@@ -24,12 +28,12 @@ module.exports = class userController {
       const user = {
         name,
         email,
-        cpf,
+        cpf: cpfFormat(cpf),
         password,
       }
 
       checkMissingParams(user, userMock)
-      await checkInvalidParams(user)
+      await signupInvalidParams(user)
       await passwordValidator(user.password)
       await cpfValidator(user.cpf)
       await emailValidator(user.email)
@@ -40,12 +44,49 @@ module.exports = class userController {
       const userReady = new User({
         name,
         email,
-        cpf: cpfFormat(user.cpf),
+        cpf: user.cpf,
         password: ph,
       })
 
       const newUser = await userReady.save()
       res.status(201).json(newUser)
+    } catch (error) {
+      res.status(400).json({ message: error.message })
+    }
+  }
+
+  static async login(req, res) {
+    const { password } = req.body
+    let { identifier } = req.body
+
+    try {
+      const userMock = {
+        id: "",
+        password: "",
+      }
+
+      if (!validator.isEmail(identifier)) {
+        identifier = cpfFormat(identifier)
+        await cpfValidator(identifier)
+      } else {
+        await emailValidator(identifier)
+      }
+
+      let user = {
+        id: identifier,
+        password,
+      }
+
+      checkMissingParams(user, userMock)
+      await loginInvalidParams(user.id)
+
+      const userDb = await User.findOne({
+        $or: [{ email: identifier }, { cpf: identifier }],
+      })
+
+      await checkPassword(user.password, userDb.password)
+
+      await createUserToken(user, req, res)
     } catch (error) {
       res.status(400).json({ message: error.message })
     }
