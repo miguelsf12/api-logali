97ffe93df23ca12ae8a2d2912c0ea1dc1da89d32
Miguel Santos <miguelsantosf12@gmail.com>
2024-09-04 16:35:18 -0300

    feat: middleware validação de token

---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken")
+const getToken = require("../helpers/get-token")
+const UnauthorizedError = require("../errors/unauthorized-error")
+
+const auth = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json(new UnauthorizedError())
+  }
+
+  const token = getToken(req)
+
+  if (!token) {
+    return res.status(401).json(new UnauthorizedError())
+  }
+
+  try {
+    const verified = jwt.verify(token, "para o sucesso, basta apenas começar")
+    req.user = verified
+    next()
+  } catch (err) {
+    return res.status(400).json(new UnauthorizedError())
+  }
+}
+
+module.exports = auth
