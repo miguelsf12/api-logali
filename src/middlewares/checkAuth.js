4d707d452272d2a43a3cf90eb16e912f736bcd48
Miguel Santos <miguelsantosf12@gmail.com>
2024-09-09 16:53:27 -0300

    docs: renomeado

--- src/middlewares/checkAuth.js
+++ src/middlewares/checkAuth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 const getToken = require("../helpers/get-token")
 const UnauthorizedError = require("../errors/unauthorized-error")
 
-const auth = (req, res, next) => {
+const checkAuth = (req, res, next) => {
   if (!req.headers.authorization) {
     return res.status(401).json(new UnauthorizedError())
   }
@@ -22,4 +22,4 @@ const auth = (req, res, next) => {
   }
 }
 
-module.exports = auth
+module.exports = checkAuth
