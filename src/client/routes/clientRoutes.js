6ae7810e3e948eb5a8dbe599f08b410fb79a3bd9
Miguel Santos <miguelsantosf12@gmail.com>
2024-09-17 18:03:24 -0300

    feat: rota de enviar localização atual

--- src/client/routes/clientRoutes.js
+++ src/client/routes/clientRoutes.js
@@ -3,5 +3,6 @@ const checkAuth = require("../../middlewares/checkAuth")
 const clientController = require("../controllers/clientController")
 
 router.post("/edit/:id", checkAuth, clientController.edit)
+router.post("/send-actual-location", checkAuth, clientController.sendActualLocation)
 
 module.exports = router
