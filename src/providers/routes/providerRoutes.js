afed4f19b3f2bc0dc71eb33787eaaad0f049f5ff
Miguel Santos <miguelsantosf12@gmail.com>
2024-09-19 17:18:46 -0300

    refactor: rota de edição alterada para patch

--- src/providers/routes/providerRoutes.js
+++ src/providers/routes/providerRoutes.js
@@ -4,7 +4,7 @@ const providerController = require("../controllers/providerController")
 const router = require("express").Router()
 
 router.post("/addservice", checkAuth, providerController.addService)
-router.post("/editservice/:id", checkAuth, providerController.editService)
+router.patch("/editservice/:id", checkAuth, providerController.editService)
 router.delete("/removeservice/:id", checkAuth, providerController.removeService)
 
 module.exports = router
