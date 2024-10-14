98f102e2e07d54c5dc2d1e29229c3c721140ba5f
Miguel Santos <miguelsantosf12@gmail.com>
2024-10-14 17:21:41 -0300

    feat: middleware de upload de imagens add

--- src/providers/routes/providerRoutes.js
+++ src/providers/routes/providerRoutes.js
@@ -1,9 +1,15 @@
 const checkAuth = require("../../middlewares/checkAuth")
+const upload = require("../../middlewares/multer")
 const providerController = require("../controllers/providerController")
 
 const router = require("express").Router()
 
-router.post("/addservice", checkAuth, providerController.addService)
+router.post(
+  "/addservice",
+  upload.array("images"),
+  checkAuth,
+  providerController.addService
+)
 router.patch("/editservice/:id", checkAuth, providerController.editService)
 router.delete("/removeservice/:id", checkAuth, providerController.removeService)
 
