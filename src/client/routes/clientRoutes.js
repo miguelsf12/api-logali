9dca1cb795b27f0e6cc7c5a84bffe7027f4fe65f
Miguel Santos <miguelsantosf12@gmail.com>
2024-10-14 17:19:41 -0300

    refactor: middleware auth add get user

--- src/client/routes/clientRoutes.js
+++ src/client/routes/clientRoutes.js
@@ -4,6 +4,6 @@ const clientController = require("../controllers/clientController")
 
 router.post("/edit/:id", checkAuth, clientController.edit)
 router.post("/send-actual-location", checkAuth, clientController.sendActualLocation)
-router.get("/get-user-profile", clientController.getUserProfile)
+router.get("/get-user-profile", checkAuth, clientController.getUserProfile)
 
 module.exports = router
