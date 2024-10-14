ae660bc149962d93f85ff83178a80e48382480b0
Miguel Santos <miguelsantosf12@gmail.com>
2024-10-14 17:22:47 -0300

    feat: rotas de resgate com base no id e usuario criadas

--- src/service/routes/serviceRoutes.js
+++ src/service/routes/serviceRoutes.js
@@ -6,5 +6,7 @@ const router = require("express").Router()
 router.get("/get-all-services", checkAuth, serviceController.getAllServices)
 router.get("/get-services-filtered", checkAuth, serviceController.getServicesFiltered)
 router.get("/get-routes-to-service/:id", checkAuth, serviceController.getRoutesToServices)
+router.get("/get-service-by-id/:id", checkAuth, serviceController.getServiceById)
+router.get("/get-my-service", checkAuth, serviceController.getMyService)
 
 module.exports = router
