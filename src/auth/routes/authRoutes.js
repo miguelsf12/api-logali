d8994f13ab9bb40a65d917a416d8b9c839c088e6
Miguel Santos <miguelsantosf12@gmail.com>
2024-10-14 17:16:50 -0300

    feat: rota checkAuth criada

--- src/auth/routes/authRoutes.js
+++ src/auth/routes/authRoutes.js
@@ -3,5 +3,6 @@ const authController = require("../controllers/authController")
 
 router.post("/register", authController.register)
 router.post("/login", authController.login)
+router.get("/check-auth", authController.checkAuth)
 
 module.exports = router
