a22c999deb1affc1cb26beb10e2a66be160635ea
Miguel Santos <miguelsantosf12@gmail.com>
2024-10-30 17:17:18 -0300

    refactor: removido destino local de imagens

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -3,9 +3,6 @@ const path = require("path")
 
 // Definindo o storage
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "src/public/service/images")
-  },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname))
   },
