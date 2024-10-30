4614464c644efae6a9c15720c07b91b4aa6671e4
Miguel Santos <miguelsantosf12@gmail.com>
2024-10-30 17:16:53 -0300

    feat: add multer na rota de ediçao de usuario

--- src/client/routes/clientRoutes.js
+++ src/client/routes/clientRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router()
 const checkAuth = require("../../middlewares/checkAuth")
 const clientController = require("../controllers/clientController")
+const upload = require("../../middlewares/multer")
 
-router.post("/edit/:id", checkAuth, clientController.edit)
+router.patch("/edit/:id", upload.single('image'), checkAuth, clientController.editUser)
 router.post("/send-actual-location", clientController.sendActualLocation)
 router.get("/get-user-profile", checkAuth, clientController.getUserProfile)
 
