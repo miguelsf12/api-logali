e6603709c7d372c8ec16284787e7fd26b462fa29
Miguel Santos <miguelsantosf12@gmail.com>
2024-11-04 17:38:03 -0300

    feat: rota de troca de senha add

--- src/auth/routes/authRoutes.js
+++ src/auth/routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/authController")
 router.post("/register", authController.register)
 router.post("/login", authController.login)
 router.get("/check-auth", authController.checkAuth)
+router.patch("/change-password", authController.changePassword)
 
 module.exports = router
