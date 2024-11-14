642ce5086886ce3d09658ae9168e54608ca993eb
Miguel Santos <miguelsantosf12@gmail.com>
2024-11-14 18:37:47 -0300

    Corrigir script start e configurar Procfile

--- src/app.js
+++ src/app.js
@@ -27,7 +27,8 @@ cloudinary.config({
 
 if (process.env.NODE_ENV !== "test") {
   // Só inicia o servidor se não for ambiente de teste
-  app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000")
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
   })
 }
